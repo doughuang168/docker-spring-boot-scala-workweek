9a09fae3067dc9592d22ee7aaf2a4bb80f65efa4
Doug Huang <doug.huang@gmail.com>
2016-05-16 16:59:27 -0700

    clean up code, remove un-needed comment

--- src/main/scala/microservice/domain/Workweek.scala
+++ src/main/scala/microservice/domain/Workweek.scala
@@ -6,28 +6,7 @@ import java.lang.Long
 import javax.persistence.Entity
 import scala.beans.BeanProperty
 import org.hibernate.validator.constraints.NotEmpty
-//
-//@Entity
-//class Workweek {
-//
-//  @Id
-//  @GeneratedValue
-//  @BeanProperty
-//  var id: Long = _
-//
-//  @BeanProperty
-//  @NotEmpty
-//  var name: String = _
-//
-//  @BeanProperty
-//  @NotEmpty
-//  var address: String = _
-//
-//  @BeanProperty
-//  @NotEmpty
-//  var zip: String = _
-//}
-//insert into workweek(id, year, week, day) values ('1993-11-21', '1993', '48', '1');
+
 @Entity
 class Workweek {
 
